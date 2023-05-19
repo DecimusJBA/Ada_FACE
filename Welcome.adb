fc3c54bf6a627b6bd0246bfe10e81d52f59aeee8
Brett Dahmer <bdahmerj@my.athens.edu>
2023-05-19 12:51:42 -0500

    second commit

---
+++ Welcome.adb
@@ -0,0 +1,7 @@
+
+PROCEDURE Welcome IS
+BEGIN
+Text_IO.Put_line( "Welcome to Games Programming with Ada" );
+Text_IO.New_line;
+Text_IO.Put_line( "Thanks for playing!");
+END;
---
+++ src/main.adb
@@ -0,0 +1,14 @@
+with Text_IO; -- text IO package
+
+procedure Main is
+
+   --declare variables--
+
+begin
+
+   Text_IO.Put_Line("Welcome to a sample Ada program!");
+   Text_IO.New_Line;
+   Text_IO.Put_Line("End of program!");
+
+end Main;
+
