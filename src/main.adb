ca2a6a5bcc71efe08f3250e75351d2a7ac65dc49
Brett Dahmer <bdahmerj@my.athens.edu>
2023-05-19 13:29:46 -0500

    third commit

--- src/main.adb
+++ src/main.adb
@@ -8,7 +8,15 @@ begin
 
    Text_IO.Put_Line("Welcome to a sample Ada program!");
    Text_IO.New_Line;
-   Text_IO.Put_Line("End of program!");
+   Text_IO.Put_Line("Going to draw a face!");
 
+   Text_IO.Put_Line("=============");
+   Text_IO.Put_Line("\\\\\\\\\\\");
+   Text_IO.Put_Line("|(*) | (*)|");
+   Text_IO.Put_Line("|    o    |");
+   Text_IO.Put_Line("\_________/");
+   Text_IO.Put_Line(" \xxxxxxx/");
+   Text_IO.Put_Line("=============");
+   Text_IO.Put_Line("End of program!");
 end Main;
 
